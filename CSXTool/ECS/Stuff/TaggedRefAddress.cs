0e45159b417b5beba6a0d5d07b4c30334bc25088
Crsky <crskycode@hotmail.com>
2024-02-16 18:25:12 +0800

    Update TaggedRefAddress class.

--- CSXTool/ECS/Stuff/TaggedRefAddress.cs
+++ CSXTool/ECS/Stuff/TaggedRefAddress.cs
@@ -2,5 +2,9 @@
 
 namespace CSXTool.ECS.Stuff
 {
-    public record TaggedRefAddress(string Tag, List<int> Refs);
+    public record TaggedRefAddress(string Tag, List<int> Refs)
+    {
+        public string Tag { get; set; } = Tag;
+        public List<int> Refs { get; set; } = Refs;
+    }
 }
