00ff40d82bd419628926b1f6ca65214e01633215
Crsky <crskycode@hotmail.com>
2024-02-17 00:56:26 +0800

    Add EXIHeader class.

---
+++ CSXTool/ECS/Stuff/EXIHeader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSXTool.ECS.Stuff
+{
+    public class EXIHeader
+    {
+        public uint Version;
+        public uint IntBase;
+    }
+}
